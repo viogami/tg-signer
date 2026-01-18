51de96281bf9c519465ad794e03de339b40a0a6a
viogami <2654613995@qq.com>
2026-01-18 16:59:51 +0800

    启用 webgui 启动命令并安装相关依赖

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ pip install -U tg-signer
 sleep infinity
 
 # 启动 webgui 供容器外访问，默认容器内8080端口
-# pip install "tg-signer[gui]"
-# tg-signer webgui -H 0.0.0.0
+pip install "tg-signer[gui]"
+tg-signer webgui -H 0.0.0.0
